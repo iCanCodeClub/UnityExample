bab1386d3eed671047b2e3b15fdc3d0a721b2d1b
iCanCodeClub <info@icancodeclub.com>
2016-10-29 16:01:51 -0400

    Initial Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System;
+
+public class CameraController : MonoBehaviour {
+	public GameObject player;
+	Vector3 offset;
+
+	void Start () {
+		offset = transform.position;
+	}
+
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
